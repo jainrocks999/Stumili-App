5215022df79f7f47badaaf164b2c99a5e7a02db8
Raju Barde <rajubarde587@gmail.com>
2025-11-15 11:48:54 +0530

    chages

--- android/app/src/main/java/com/stumili/Sound/SoundPackage.kt
+++ android/app/src/main/java/com/stumili/Sound/SoundPackage.kt
@@ -6,6 +6,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
 class SoundPackage : ReactPackage {
+
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
         // Make sure SoundModule implements ReactContextBaseJavaModule
         return listOf<NativeModule>(SoundModule(reactContext))
